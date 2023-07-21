1777879b2ec1f63853ffed9534a46e26475f906e
Sean Finley <finleysean9@gmail.com>
2023-07-20 21:56:38 -0400

    Created function to prompt to ask user about uppercase letters in the password

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLengthPrompt;
+var uppercasePrompt;
 
 // Write password to the #password input
 function writePassword() {
@@ -11,23 +13,44 @@ function writePassword() {
 
 }
 
-function generatePassword () {
-  length ();
+var lengthPrompt = "Please type a number between 8 and 128 to determine the length of the password.";
+var upperPrompt= "Would you like to use Uppercase letters in your password? (Type yes or no)";
+
+function criteriaPrompt(criteria) {
+  return prompt (criteria);
 }
 
 // Created a function to determine input for the length variable
 // It takes the input and unless it falls in the condition it calls it again
 // If the answer falls in the correct catagory it will reassign it to a different variable and return that
 function length () {
-  var passwordLengthPrompt = prompt ("Please type a number between 8 and 128 to determine the length of the password.");
+  let passwordLengthPrompt = criteriaPrompt (lengthPrompt);
   if (passwordLengthPrompt > 128 && passwordLengthPrompt < 8) {
     length ();
   } else if (passwordLengthPrompt >= 8 && passwordLengthPrompt <= 128) {
-    passwordLengthPrompt = passwordLength;
-    return passwordLength;
+    return passwordLengthPrompt;
   } else {
     length ();
   }
+}
+
+function uppercase () {
+  let uppercasePrompt = criteriaPrompt (upperPrompt);
+  if (uppercasePrompt == "yes" || uppercasePrompt == "Yes") {
+    return uppercasePrompt;
+  } else if (uppercasePrompt == "no" || uppercasePrompt == "No") {
+    return uppercasePrompt;
+  } else {
+    uppercase ();
+  }
+}
+
+function generatePassword () {
+  var x = length ();
+  if (x >= 8 && x <= 128) {
+    uppercase ();
+  }
+  return;
 }
 
 // Add event listener to generate button
